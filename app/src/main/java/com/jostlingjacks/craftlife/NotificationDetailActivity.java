4e4794e3c9d2346238a350cc044caba0515ab7ce
xlin0001 <xlin0003@student.monash.edu>
2018-09-02 16:51:15 +1000

    Merge branches 'Walkthrough' and 'master' of https://github.com/skwo0001/CraftLife into Walkthrough
    # Conflicts:
    #	.idea/assetWizardSettings.xml
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/jostlingjacks/craftlife/MainActivity.java
    #	app/src/main/java/com/jostlingjacks/craftlife/NotificationDetailActivity.java
    #	app/src/main/res/layout/activity_notification_detail.xml
    #	app/src/main/res/menu/activity_main_drawer.xml

--- app/src/main/java/com/jostlingjacks/craftlife/NotificationDetailActivity.java
+++ app/src/main/java/com/jostlingjacks/craftlife/NotificationDetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -119,7 +121,7 @@ public class NotificationDetailActivity extends AppCompatActivity {
             Intent myIntent = new Intent(Intent.ACTION_SEND);
             myIntent.setType("text/plain");
             String shareSub = getName();
-            String shareBody = "Details: " + getDescription() + "/n Address: " + getAddress();
+            String shareBody = "Details: " + getDescription() + "\n Address: " + getAddress();
             myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
             myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
             startActivity(Intent.createChooser(myIntent, "Share using"));
