ab8188131ce8ce273b16f2876a0fca79b6521925
szeyankwok <skwo0001>
2018-09-29 21:22:23 +1000

    Merge branch 'NotificatinIcons' of https://github.com/skwo0001/CraftLife into canada
    # Conflicts:
    #	app/src/main/java/com/jostlingjacks/craftlife/MainActivity.java
    #	app/src/main/java/com/jostlingjacks/craftlife/SendRequest.java

--- app/src/main/java/com/jostlingjacks/craftlife/DataBaseHelper.java
+++ app/src/main/java/com/jostlingjacks/craftlife/DataBaseHelper.java
@@ -277,7 +277,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     }
 
-    //Return title, description, address, option form the user, latitude and longitude
+    //Return title, description, address, option from the user, latitude and longitude
     public Cursor getAddress (String email){
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor mCursor = db.query(true,SUGGESTION_TABLE, new String[] {T2_COL_4,T2_COL_5,T2_COL_6,T2_COL_9,T2_COL_10,T2_COL_11}, T2_COL_2 + "=? and " + T2_COL_3 + "!=? ", new String[] {email,"regular"},
