d399f1200d07ca1124f1b844196539c24f00aa6a
szeyankwok <skwo0001>
2018-10-08 16:41:12 +1100

    Merge branch 'ArtPlaceCollection' of https://github.com/skwo0001/CraftLife into notificationChannel
    # Conflicts:
    #	app/src/main/java/com/jostlingjacks/craftlife/DataBaseHelper.java

--- app/src/main/java/com/jostlingjacks/craftlife/DataBaseHelper.java
+++ app/src/main/java/com/jostlingjacks/craftlife/DataBaseHelper.java
@@ -140,7 +140,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
         //select title,details,address,time from suggestion s where s.email = email and s.type = type;
         Cursor mCursor = db.query(SUGGESTION_TABLE,new String[] {T2_COL_4,T2_COL_5,T2_COL_6,T2_COL_7,T2_COL_13}, T2_COL_2 + "=? and "  + T2_COL_3+"=?", new String[] {email,type},
-                null,null,null,null);
+                null,null,T2_COL_8,null);
         if (mCursor != null){
             mCursor.moveToLast();
         }
@@ -196,7 +196,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
                 null,null,T2_COL_8 + " DESC",null);
 
         if (mCursor != null){
-            mCursor.moveToNext();
+            mCursor.moveToFirst();
         }
         return mCursor;
     }
@@ -209,7 +209,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
                 null,null,T2_COL_8 + " DESC",null);
 
         if (mCursor != null){
-            mCursor.moveToNext();
+            mCursor.moveToFirst();
         }
         return mCursor;
     }
